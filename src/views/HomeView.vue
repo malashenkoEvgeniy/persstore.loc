f9401b4a8316059ba3b620bc7bea21f119441192
malashenkoEvgeniy <111k1808@gmail.com>
2022-10-17 19:57:42 +0300

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    Hello wold
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted () {
+    this.getPoint()
+  },
+  methods: {
+    getPoint () {
+      this.axios.get('http://localhost/api')
+        .then(res => {
+          console.log(res)
+        })
+    }
+  }
+}
+</script>
